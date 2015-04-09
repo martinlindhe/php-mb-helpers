942887a17fa64f5316eaf00a3963b12bcbedf83b
Martin Lindhe <martin@ubique.se>
2015-04-09 17:20:16 +0200

    add function_exists() checks to avoid future issues

--- src/mb_helpers.php
+++ src/mb_helpers.php
@@ -1,36 +1,42 @@
 <?php
 
-/**
- * @param string $s
- * @param string $encoding
- * @return string Uc Words
- */
-function mb_ucwords($s, $encoding = 'UTF-8')
-{
-    return mb_convert_case($s, MB_CASE_TITLE, $encoding);
+if (!function_exists('mb_ucwords')) {
+    /**
+     * @param string $s
+     * @param string $encoding
+     * @return string Uc Words
+     */
+    function mb_ucwords($s, $encoding = 'UTF-8')
+    {
+        return mb_convert_case($s, MB_CASE_TITLE, $encoding);
+    }
 }
 
-/**
- * @param string $s
- * @param string $encoding
- * @return string Uc first
- */
-function mb_ucfirst($s, $encoding = 'UTF-8')
-{
-    $firstLetter = mb_substr($s, 0, 1, $encoding);
-    $rest = mb_substr($s, 1, mb_strlen($s));
+if (!function_exists('mb_ucfirst')) {
+    /**
+     * @param string $s
+     * @param string $encoding
+     * @return string Uc first
+     */
+    function mb_ucfirst($s, $encoding = 'UTF-8')
+    {
+        $firstLetter = mb_substr($s, 0, 1, $encoding);
+        $rest = mb_substr($s, 1, mb_strlen($s));
 
-    return mb_strtoupper($firstLetter, $encoding) . $rest;
+        return mb_strtoupper($firstLetter, $encoding) . $rest;
+    }
 }
 
-/**
- * @param string $str
- * @param string $encoding
- * @return string
- */
-function mb_strrev($str, $encoding = 'UTF-8')
-{
-    $str = mb_convert_encoding($str, 'UTF-16BE', $encoding);
+if (!function_exists('mb_strrev')) {
+    /**
+     * @param string $str
+     * @param string $encoding
+     * @return string
+     */
+    function mb_strrev($str, $encoding = 'UTF-8')
+    {
+        $str = mb_convert_encoding($str, 'UTF-16BE', $encoding);
 
-    return mb_convert_encoding(strrev($str), $encoding, 'UTF-16LE');
+        return mb_convert_encoding(strrev($str), $encoding, 'UTF-16LE');
+    }
 }
